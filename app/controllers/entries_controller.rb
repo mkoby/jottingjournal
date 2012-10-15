e6e46f5343e82d866fdd536d71a46ca106a4e51b
Michael Koby <michael@mkoby.com>
2012-10-15 15:59:47 -0500

    Basic new entry form

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,8 +1,20 @@
 class EntriesController < ApplicationController
+  before_filter :authenticate_user!
+
   def new
+    @entry ||= current_user.entries.new
   end
 
   def create
+    @entry = current_user.entries.new(params[:entry])
+
+    if @entry.save
+      flash[:notice] = "Entry saved"
+      redirect_to root_path
+    else
+      flash[:alert] = "Oops! Something went wrong, please try agagin. If the problem persists please let us know."
+      render :new
+    end
   end
 
   def show
