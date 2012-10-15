f42534b240989f24540a04d6cb95cc33702fad7d
Michael Koby <michael@mkoby.com>
2012-10-15 15:29:09 -0500

    Add method to show devise alerts correctly

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def display_flash(name,msg)
+    case name.to_s
+    when "notice" then
+      content_tag :div, msg, :class => "alert alert-success"
+    when "alert" then
+      content_tag :div, msg, :class => "alert alert-error"
+    else
+      content_tag :div, msg, :class => "alert alert-info"
+    end
+  end
 end
