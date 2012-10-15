b38a4b9830a03498a61f8601ffc3fd5b222238e3
Michael Koby <michael@mkoby.com>
2012-10-15 15:28:43 -0500

    Add Entries controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Jotjournal::Application.routes.draw do
 
+  devise_for :users
+  root :to => 'home#index'
   get "home/index"
 
-  devise_for :users
-
-  root :to => 'home#index'
+  resources :entries
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
