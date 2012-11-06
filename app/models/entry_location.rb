647f5c8ea9fcdc67d9f096953a6c9f13ab4039f7
Michael Koby <michael@mkoby.com>
2012-11-06 10:36:49 -0600

    Add method that builds link to map for entry location

--- app/models/entry_location.rb
+++ app/models/entry_location.rb
@@ -3,6 +3,8 @@ class EntryLocation < ActiveRecord::Base
   attr_accessible :latitude, :longitude
   attr_writer :address
 
+  MAP_URL_BASE = "https://maps.google.com/?q="
+
   reverse_geocoded_by :latitude, :longitude  do |obj, results|
     if geo = results.first
       obj.city = geo.city
@@ -12,4 +14,11 @@ class EntryLocation < ActiveRecord::Base
   end
 
   after_validation :reverse_geocode
+
+  def map_link
+    map_query_string = self.zipcode ? self.zipcode : "#{self.city},#{self.state}"
+    map_query_string = "#{self.latitude},#{self.longitude}" if map_query_string.nil? || map_query_string == ","
+
+    return MAP_URL_BASE + map_query_string
+  end
 end
