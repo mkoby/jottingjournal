dd9e244a89c68407d5cd1aee7c5f90b25025b878
Michael Koby <michael@mkoby.com>
2012-11-23 16:35:38 -0600

    Force www subdomain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :ensure_domain
+
+  private
+
+  def ensure_domain
+    if request.env['HTTP_HOST'] == "jottingjournal.com"
+      redirect_to_full_url "//www.jottingjournal.com#{request.request_uri}", 301
+    end
+  end
 end
